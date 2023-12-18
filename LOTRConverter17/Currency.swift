a9f16902c5f4380e85c72920f527b466a16fe30f
Antun Karlovac <antun@antunkarlovac.com>
2023-12-17 17:23:20 -0800

    Lesson 11 model for currency starting point

---
+++ LOTRConverter17/Currency.swift
@@ -0,0 +1,14 @@
+//
+//  Currency.swift
+//  LOTRConverter17
+//
+//  Created by Antun Karlovac on 12/17/23.
+//
+
+enum Currency: Double {
+    case copperPenny = 6400
+    case silverPenny = 64
+    case silverPiece = 16
+    case goldPenny = 4
+    case goldPiece = 1
+}
